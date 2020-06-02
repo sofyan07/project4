2a50605cc59ec429d8dffe567dd0baab34a9fd0c
sofyan07 <brosofyan23@gmail.com>
2020-06-02 16:10:15 +0700

    project 4

---
+++ phpmvc/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'Sofyan';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="mt-4 container">
+    <h1>About Me</h1>
+    <img src="<?= BASEURL; ?>/img/7.jpg" alt="Yanz"width="200"
+    class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun,  saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+</div>
